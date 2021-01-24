4eb5e89d19586e5823ef05f4ccfb579191b34b60
Domagoj Tadin <domagoj.tadin@gmail.com>
2021-01-23 20:17:23 -0500

    updated var naming to be more convention friendly

--- rock_scissors_paper/scripts/rsp.js
+++ rock_scissors_paper/scripts/rsp.js
@@ -1,56 +1,55 @@
-function computerPlay() {
-  let turn;
-  var turnval = Math.floor(Math.random() * 100);
-  switch (true) {
-    case turnval >= 0 && turnval < 33:
-      turn = "rock";
-      break;
-    case turnval >= 33 && turnval < 66:
-      turn = "scissors";
-      break;
-    default:
-      turn = "paper";
-  }
-  return turn;
+function computerTurn() {
+	let turnResult;
+	let randomNumber = Math.floor(Math.random() * 100);
+	switch (true) {
+		case randomNumber >= 0 && turnval < 33:
+			turnResult = 'rock';
+			break;
+		case randomNumber >= 33 && turnval < 66:
+			turnResult = 'scissors';
+			break;
+		default:
+			turnResult = 'paper';
+	}
+	return turnResult;
 }
 
 function playRound(playerInput, computerInput) {
-  let outcome;
-  let result;
-  switch (true) {
-    case playerInput.toLowerCase() == computerInput.toLowerCase():
-      outcome = `Tie game! ${playerInput} equals ${computerInput}`;
-      result = "tie";
-      break;
-    case playerInput.toLowerCase() == "rock" && computerInput.toLowerCase() == "scissors":
-    case playerInput.toLowerCase() == "scissors" && computerInput.toLowerCase() == "paper":
-    case playerInput.toLowerCase() == "paper" && computerInput.toLowerCase() == "rock":
-      outcome = `You win! ${playerInput} beats ${computerInput}`;
-      result = "player win"
-      break;
-    case playerInput.toLowerCase() == "scissors" && computerInput.toLowerCase() == "rock":
-    case playerInput.toLowerCase() == "paper" && computerInput.toLowerCase() == "scissors":
-    case playerInput.toLowerCase() == "rock" && computerInput.toLowerCase() == "paper":
-      outcome = `You lose! ${computerInput} beats ${playerInput}`;
-      result = "computer win"
-      break;
-    default:
-      outcome = `You CHEAT! Lizard always beats ${playerInput}`;
-      result = "computer win"
-  };
-  return {outcome, result};
+	let outcome;
+	let result;
+	switch (true) {
+		case playerInput.toLowerCase() == computerInput.toLowerCase():
+			outcome = `Tie game! Your ${playerInput} equals their ${computerInput}`;
+			result = 'tie';
+			break;
+		case playerInput.toLowerCase() == 'rock' && computerInput.toLowerCase() == 'scissors':
+		case playerInput.toLowerCase() == 'scissors' && computerInput.toLowerCase() == 'paper':
+		case playerInput.toLowerCase() == 'paper' && computerInput.toLowerCase() == 'rock':
+			outcome = `You win! Your ${playerInput} beats their ${computerInput}`;
+			result = 'player win';
+			break;
+		case playerInput.toLowerCase() == 'scissors' && computerInput.toLowerCase() == 'rock':
+		case playerInput.toLowerCase() == 'paper' && computerInput.toLowerCase() == 'scissors':
+		case playerInput.toLowerCase() == 'rock' && computerInput.toLowerCase() == 'paper':
+			outcome = `You lose! Their ${computerInput} beats your ${playerInput}`;
+			result = 'computer win';
+			break;
+		default:
+			outcome = `You CHEAT! Lizard always beats your ${playerInput}`;
+			result = 'computer win';
+	}
+	return { outcome, result };
 }
 
-function game(){
-    let result = [];
-    console.log(result.length)
-    while(result.length<5){
-        playerTurn = window.prompt("Enter 'rock', 'scissors', or 'paper'")
-        let roundresult = playRound(playerTurn,computerPlay())
-        console.log(roundresult.outcome)
-        result.push(roundresult.result)
-    }
-    result.forEach(element => {
-        console.log(element+'\n')   
-    });  
+function game() {
+	let result = [];
+	while (result.length < 5) {
+		playerTurn = window.prompt("Enter 'rock', 'scissors', or 'paper'");
+		let roundresult = playRound(playerTurn, computerTurn());
+		console.log(roundresult.outcome);
+		result.push(roundresult.result);
+	}
+	result.forEach((element) => {
+		console.log(element + '\n');
+	});
 }
